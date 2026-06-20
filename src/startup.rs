fd2c1661afba4061150e1e1ee208939ceaec99a9
Jeff Mitchell <crusty.rustacean@gmail.com>
2026-06-19 21:52:45 -0700

    feat: add SQLite database to app start up and tests

--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,14 @@
 // src/startup.rs
 
 // dependencies
-use crate::configuration::Settings;
+use crate::configuration::{DatabaseSettings, Settings};
 use crate::routes::health_check;
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web, web::Data};
+use sqlx::SqlitePool;
+use sqlx::sqlite::SqliteConnectOptions;
 use std::net::TcpListener;
+use std::str::FromStr;
 use tracing_actix_web::TracingLogger;
 
 pub struct Application {
@@ -19,9 +22,10 @@ impl Application {
             "{}:{}",
             configuration.application.host, configuration.application.port
         );
+        let db_pool = create_database_pool(configuration.database).await?;
         let listener = TcpListener::bind(address)?;
         let port = listener.local_addr()?.port();
-        let server = run(listener, configuration.application.base_url).await?;
+        let server = run(listener, db_pool, configuration.application.base_url).await?;
         Ok(Self { port, server })
     }
 
@@ -34,15 +38,31 @@ impl Application {
     }
 }
 
+async fn create_database_pool(
+    db_configuration: DatabaseSettings,
+) -> Result<SqlitePool, anyhow::Error> {
+    let db_path = format!("sqlite:{}", db_configuration.path);
+    let options = SqliteConnectOptions::from_str(&db_path)?.create_if_missing(true);
+    let pool = SqlitePool::connect_with(options).await?;
+
+    Ok(pool)
+}
+
 pub struct ApplicationBaseUrl(pub String);
 
-async fn run(listener: TcpListener, base_url: String) -> Result<Server, anyhow::Error> {
+async fn run(
+    listener: TcpListener,
+    pool: SqlitePool,
+    base_url: String,
+) -> Result<Server, anyhow::Error> {
     let base_url = Data::new(ApplicationBaseUrl(base_url));
+    let db_pool = Data::new(pool);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .app_data(base_url.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
