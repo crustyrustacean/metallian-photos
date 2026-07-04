94136226419b42397a7983cdaffdd6d7ccc3c78a
Jeff Mitchell <crusty.rustacean@gmail.com>
2026-07-03 22:03:17 -0700

    improvement: database delete now also deletes the corresponding photo in R2

--- src/routes/delete.rs
+++ src/routes/delete.rs
@@ -2,6 +2,7 @@
 
 // dependencies
 use crate::database::DatabaseBackend;
+use crate::storage::StorageBackend;
 use crate::utils::e400;
 use actix_web::HttpResponse;
 use actix_web::http::StatusCode;
@@ -12,10 +13,12 @@ use uuid::Uuid;
 pub async fn delete(
     path: Path<String>,
     database: Data<Box<dyn DatabaseBackend>>,
+    storage: Data<Box<dyn StorageBackend>>,
 ) -> Result<HttpResponse, actix_web::Error> {
     let id_string = path.into_inner();
     let id = Uuid::parse_str(&id_string).map_err(e400)?;
     database.delete(id).await?;
+    storage.delete(id).await?;
 
     Ok(HttpResponse::Ok().status(StatusCode::NO_CONTENT).finish())
 }
